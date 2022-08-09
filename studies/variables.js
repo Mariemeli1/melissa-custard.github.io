420ede1afacb24377d923352887e1651fbf9b5d1
Melissa Custard <custardmelissa1@gmail.com>
2022-08-09 16:17:06 +0000

    variables studies

---
+++ studies/variables.js
@@ -0,0 +1,24 @@
+/**
+ * VARIABLES:
+ * 
+ * 0.Variables are containers for storing data values that can be changed later on.
+ * Variables are how programmers give a name to a value so that we can reuse it, update it or simply keep track of it.
+ * 1. To create a variable we can use the keywords var, let or const.
+ * 2. You need to give a variable a name, names are case sensitive and can contain a letter or these 2 symbols $ or _
+ * they can also contain numbers but they cannot start with a number.
+ * 3. There are 2 things that need to take place in order to use a variable: Declaration and assignment/initialization.
+ */
+
+
+// 1. Declaration //
+// To declare a variable you need the var keyword and the name of the variable
+var favColor;
+console.log(favColor); //-----> Prints out undefined because the var hasn't been assigned to any value.
+
+// 2. Assignment/Initialization //
+// Assigning a variable means giving the variable a value using an equal sign.
+var favColor = 'pink';
+console.log(favColor); //-----> Prints pink because that is the value assigned to the variable favColor.
+
+//3. var/let/const //
+//var
