f809dcc3d0ef07e02fe311db730039d5b63d6530
Melissa Custard <custardmelissa1@gmail.com>
2022-08-13 03:52:40 +0000

    chessboard annoying

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -37,18 +37,24 @@ function fizzBuzz() {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard(num) {
+function drawChessboard() {
   let size = 8;
-  let emptyStr = "";
-  for(let i = 0; i < num.length; i++){
-    for(let x = 0; x < num.length; x++){
-     if((i + x) % 2 === 0){
-       emptyStr
-     }
+  let board = "";
+  for(let i = 0; i < size; i++){
+    for(let x = 0; x < size; x++){
+      if ((i + x) % 2 == 0){
+        board += " ";
+      } else {
+        board += '#';
+      }
+         
+       
     }
+    board += "\n";
   }
 }
 
+console.log(board);
     
 
 ////////////////////////////////////////////////////////////////////////////////
