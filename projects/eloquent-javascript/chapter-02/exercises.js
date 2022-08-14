eb85cdcf0ddaf1c46e327646426007fedd881fcc
Melissa Custard <custardmelissa1@gmail.com>
2022-08-14 21:04:19 +0000

    eloquents js

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -37,24 +37,24 @@ function fizzBuzz() {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
+function drawChessboard(size) {
   let size = 8;
   let board = "";
   for(let i = 0; i < size; i++){
     for(let x = 0; x < size; x++){
-      if ((i + x) % 2 == 0){
+      if (i + x % 2 === 0){
         board += " ";
       } else {
         board += '#';
       }
-         
-       
     }
     board += "\n";
   }
+  console.log(board);
 }
 
-console.log(board);
+
+
     
 
 ////////////////////////////////////////////////////////////////////////////////
--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -16,7 +16,7 @@ function search(arr, str){
      for(var i = 0; i < arr.length; i++){
         if(arr[i].name === str){
             return arr[i];
-       }
+        }
     }
   return null;
     
