0f6ad2d35af1b8c57f299f62c4da3880016a8afc
Melissa Custard <custardmelissa1@gmail.com>
2022-08-22 21:32:03 +0000

    recursion 1st

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -5,26 +5,57 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+  if(n === 0){
+    return 1;
+  }
+  if(n <= 0){
+    return null;
+  }
+  else{
+    return n * factorial(n - 1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  if(array.length === 0){
+    return 0;
+  }
+  return  array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-};
+  if(n === -1){
+    return false;
+  }
+  else if(n === 0){
+    return true;
+  
+  } 
+  else if(n > 0){
+    return isEven(n - 2);
+  }
+  
+  else{
+   return isEven(n + 2);
+  }
+}
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+    if(n === -1){
+    return sumBelow
+  }
 };
 
 // 6. Get the integers in range (x, y).
@@ -38,6 +69,9 @@ var range = function(x, y) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if(exp === 0){
+    return 1;
+  }
 };
 
 // 8. Determine if a number is a power of two.
