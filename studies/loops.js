fcd34f5233944500a01c07867e2698472914a1fa
Melissa Custard <custardmelissa1@gmail.com>
2022-08-31 04:29:32 +0000

    loopy doopy

---
+++ studies/loops.js
@@ -0,0 +1,10 @@
+/**
+ * LOOPS:
+ * 
+ * 0.Loops are used in JavaScript to perform repeated tasks based on a condition. 
+ * Conditions typically return true or false.
+ * A loop will continue running until the defined condition returns false.
+ * 1. For loops need a initial expression, a condition expression, a increment expression and a statement.
+ * For loops loop through a block of code a number of times
+ * 2. 
+ */
